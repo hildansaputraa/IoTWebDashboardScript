c2b4b6d1092685ee505ff1e701542dd98fd88b49
hildansaputraaa <138800894+hildansaputraaa@users.noreply.github.com>
2025-11-03 20:04:59 +0700

    Update dashboard.php

--- page/dashboard.php
+++ page/dashboard.php
@@ -80,20 +80,7 @@ $result = mysqli_query($connection, $sql);
                 <!-- /.card-body -->
               </div>
             </div>
-            
-            <!-- Tambahkan gambar di sini (sebelah kanan Lamp Button) -->
-            <div class="col-lg-6">
-              <div class="card card-info">
-                <div class="card-header">
-                  <h3 class="card-title">System Overview</h3>
-                </div>
-                <div class="card-body text-center">
-                  <img src="images/system-diagram.jpg" alt="System Diagram" class="img-fluid" style="max-height: 200px;">
-                  <p class="mt-2">IoT System Architecture</p>
-                </div>
-              </div>
-            </div>
-          </div>
+      
           
           <!-- Bagian Sensor Data (Node 1-4) -->
           <div class="row">
