f6f51ebe6fe8dfc9853ae97b4278b28ca4018464
hildansaputraaa <138800894+hildansaputraaa@users.noreply.github.com>
2025-11-03 20:20:34 +0700

    Update dashboard.php

--- page/dashboard.php
+++ page/dashboard.php
@@ -157,15 +157,14 @@ $result = mysqli_query($connection, $sql);
             <!-- Tambahkan gambar di bawah Sensor Data -->
             <div class="row">
               <div class="col-lg-12">
-                <div class="card card-default">
+                <div class="card card bg-gray">
                   <div class="card-header">
                     <h3 class="card-title">Monitoring Dashboard</h3>
                   </div>
                   <div class="card-body text-center">
                     <div class="row">
                       <div class="col-md-12">
-                        <img src="dist/img/maps.jpg" alt="Network Layout" class="img-fluid rounded" style="max-height: 500px;">
-                        <p class="mt-2">Network Layout Diagram</p>
+                        <img src="dist/img/maps.jpg" alt="Network Layout" class="img-fluid rounded" style="max-height: 300px;">
                       </div>
                     </div>
                   </div>
