25994146d7a7b44f3018ccdef67882b8173c9bb9
hildansaputraaa <138800894+hildansaputraaa@users.noreply.github.com>
2025-11-03 19:58:47 +0700

    Update dashboard.php

--- page/dashboard.php
+++ page/dashboard.php
@@ -80,20 +80,6 @@ $result = mysqli_query($connection, $sql);
                 <!-- /.card-body -->
               </div>
             </div>
-            
-            <!-- Tambahkan gambar di sini (sebelah kanan Lamp Button) -->
-            <div class="col-lg-6">
-              <div class="card card-info">
-                <div class="card-header">
-                  <h3 class="card-title">Maps</h3>
-                </div>
-                <div class="card-body text-center">
-                  <img src="dist\img\maps.jpg" alt="System Diagram" class="img-fluid" style="max-height: 200px;">
-                  <p class="mt-2">Node</p>
-                </div>
-              </div>
-            </div>
-          </div>
           
           <!-- Bagian Sensor Data (Node 1-4) -->
           <div class="row">
@@ -175,17 +161,12 @@ $result = mysqli_query($connection, $sql);
             <div class="col-lg-12">
               <div class="card card-default">
                 <div class="card-header">
-                  <h3 class="card-title">Monitoring Dashboard</h3>
+                  <h3 class="card-title">Maps</h3>
                 </div>
                 <div class="card-body text-center">
                   <div class="row">
                     <div class="col-md-6">
-                      <img src="images/network-layout.png" alt="Network Layout" class="img-fluid rounded" style="max-height: 250px;">
-                      <p class="mt-2">Network Layout Diagram</p>
-                    </div>
-                    <div class="col-md-6">
-                      <img src="images/sensor-placement.jpg" alt="Sensor Placement" class="img-fluid rounded" style="max-height: 250px;">
-                      <p class="mt-2">Sensor Placement Map</p>
+                      <img src="dist/img/maps.jpg" alt="Network Layout" class="img-fluid rounded" style="max-height: 250px;">
                     </div>
                   </div>
                 </div>
