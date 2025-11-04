55bd22aba02058986c5cd577441390855e681b7e
hildansaputraaa <138800894+hildansaputraaa@users.noreply.github.com>
2025-11-04 15:00:31 +0700

    Update sidebar.php

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,8 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="index3.html" class="brand-link">
-    <img src="dist/img/logo-with-title.png" alt="Logo" class="brand-image ">
-    <span class="brand-text font-weight-light">.</span>
+    <img src="dist/img/logo-with-title.png" alt="Logo" class="brand-image">
   </a>
   <!-- Sidebar -->
   <div class="sidebar">
@@ -76,7 +75,5 @@
     </nav>
     <!-- /.sidebar-menu -->
   </div>
-  </aside>
-  <!-- /.sidebar -->
-
-  
+</aside>
+<!-- /.sidebar -->
