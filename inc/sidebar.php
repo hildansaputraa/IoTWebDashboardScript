5b9e5f00d518838c2074aa9318e353f4b717996a
hildansaputraaa <138800894+hildansaputraaa@users.noreply.github.com>
2025-11-04 14:57:12 +0700

    update

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="index3.html" class="brand-link">
+    <img src="dist/img/logo-with-title.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">SYSTEM IOT</span>
   </a>
-
   <!-- Sidebar -->
   <div class="sidebar">
     <!-- Sidebar user panel (optional) -->
