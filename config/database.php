6155f6b219ba822269e24d5c59c20550646b4d58
hildansaputraaa <138800894+hildansaputraaa@users.noreply.github.com>
2025-11-04 11:41:27 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,17 +1,26 @@
 <?php
 
 $servername = "srv482.hstgr.io";
-$username = "u212736227_smartiot";
-$password = "Smartiot123.";
-$database = "u212736227_systemiot";
+$username   = "u212736227_smartiot";
+$password   = "Smartiot123.";
+$database   = "u212736227_systemiot";
 
-$connection = mysqli_connect($servername,$username,$password,$database);
+// Koneksi ke database
+$connection = mysqli_connect($servername, $username, $password, $database);
 
-//periksa koneksi
-if(!$connection){
+// Periksa koneksi
+if (!$connection) {
     die("Connection Failed: " . mysqli_connect_error());
 }
 
-// echo "Koneksi Berhasil";
+// Set timezone PHP ke Asia/Jakarta (WIB)
+date_default_timezone_set('Asia/Jakarta');
 
+// Set timezone MySQL untuk sesi ini (tidak butuh SUPER privilege)
+mysqli_query($connection, "SET time_zone = '+07:00'");
+
+// Optional: aktifkan charset UTF-8 biar aman untuk teks Indonesia
+mysqli_set_charset($connection, "utf8mb4");
+
+// echo "Koneksi Berhasil";
 ?>
