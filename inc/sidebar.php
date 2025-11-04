ca0873614a15d940b30b70271f6330dc81d55a93
hildansaputraaa <138800894+hildansaputraaa@users.noreply.github.com>
2025-11-04 22:20:48 +0700

    Update sidebar.php

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="?page=dashboard" class="brand-link">
-    <img src="../dist/img/unesa.jpg" alt="Logo" class="brand-image" style="float: none; max-height: 50px; width: auto; margin: 0 auto; display: block;">
+    <img src="../dist/img/logo-with-title.png" alt="Logo" class="brand-image" style="float: none; max-height: 50px; width: auto; margin: 0 auto; display: block;">
   </a>
   <!-- Sidebar -->
   <div class="sidebar">
